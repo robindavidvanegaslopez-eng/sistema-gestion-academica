c5424ee43b294e687331efca959a7f28a678b1be
David Vanegas y Omar Valencia <robindavidvanegaslopez@gmail.com>
2026-09-21 15:46:20 -0500

    Agrega consulta del mejor estudiante

--- estudiantes.py
+++ estudiantes.py
@@ -23,3 +23,13 @@ def eliminar_estudiante(codigo):
  estudiantes.remove(estudiante) 
  return True 
  return False 
+
+def mejor_estudiante(estudiantes): 
+ if not estudiantes: 
+ return None
+ mejor = estudiantes[0] 
+ for estudiante in estudiantes: 
+ if calcular_promedio(estudiante) > calcular_promedio(mejor): 
+ mejor = estudiante 
+ return mejor 
+ 
